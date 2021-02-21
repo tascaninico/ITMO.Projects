b653916344ec0fbd5c6639f32d26b21de11c60c5
tascaninico <73658014+tascaninico@users.noreply.github.com>
2021-02-22 02:15:19 +0300

    Add files via upload

---
+++ CS_WinForm_Labs/Lab 1/Labs 1.1-1.3/Forms1/nForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Forms1
+{
+    public partial class nForm : Forms1.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
